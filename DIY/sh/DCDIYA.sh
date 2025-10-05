1a37e4386e3b607afb7f2f6d010f0f8f50a94743
wwz09 <wwz5124@gmail.com>
2025-10-05 10:30:23 +0800

    更新 DCDIYA.sh

--- DIY/sh/DCDIYA.sh
+++ DIY/sh/DCDIYA.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #添加PY支持
 
-sed -i 's%https://gitea.com/404ynnub/resource/raw/branch/main/version.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+sed -i 's%https://gitea.com/404ynnub/resource/raw/branch/main/boxsource.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
 sed -i 's%http://www.饭太硬.net/tv/%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
 sed -i 's%http://www.饭太硬.net/tv%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
 sed -i 's%(Hawk.get(HawkConfig.API_URL, ""))%(Hawk.get(HawkConfig.API_URL, "https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/TXT/dc.json"))%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
