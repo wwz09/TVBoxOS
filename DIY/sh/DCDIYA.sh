dfcb513342a1f2e1cf7692492cbf403248fd5b0f
wwz09 <wwz5124@gmail.com>
2025-10-05 08:20:15 +0800

    sh

--- DIY/sh/DCDIYA.sh
+++ DIY/sh/DCDIYA.sh
@@ -4,11 +4,11 @@
 sed -i 's%https://gitea.com/404ynnub/resource/raw/branch/main/boxsource.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
 sed -i 's%http://www.饭太硬.net/tv/%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
 sed -i 's%http://www.饭太硬.net/tv%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/a.txt%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
+sed -i 's%(Hawk.get(HawkConfig.API_URL, ""))%(Hawk.get(HawkConfig.API_URL, "https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/TXT/dc.json"))%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+sed -i 's%url + URL.DEFAULT_STORE_API_URL%"https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/TXT/dc.json"%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+sed -i 's%url + URL.DEFAULT_API_URL%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+sed -i 's%(HawkConfig.LIVE_URL, ""))%(HawkConfig.LIVE_URL, "https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/live2.txt"))%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+sed -i 's%URL.DEFAULT_STORE_API_URL%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 sed -i 's%硬解码%软解码%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 
 echo 'DIY end'
