2f31961bb80a829e1284fef56f6aac19f80b479d
wwz09 <wwz5124@gmail.com>
2025-10-05 09:24:10 +0800

    更新 DCDIYA.sh

--- DIY/sh/DCDIYA.sh
+++ DIY/sh/DCDIYA.sh
@@ -11,7 +11,7 @@ sed -i 's%(HawkConfig.LIVE_URL, ""))%(HawkConfig.LIVE_URL, "https://git.wwz09.xi
 sed -i 's%URL.DEFAULT_STORE_API_URL%"https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json"%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 sed -i 's%// String defaultApiName = "自备份线路";%String defaultApiName = "自备份线路";%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 sed -i 's%// String defaultApi = url + URL.DEFAULT_API_URL;%String defaultApi = "https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/main.json";%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
-sed -i 's%// Map<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP%Map<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
+sed -i 's%// Map<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP, new HashMap<>());%Map<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP, new HashMap<>());%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 sed -i 's%// defaultApiMap.put(defaultApiName%defaultApiMap.put(defaultApiName%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 sed -i 's%// List<String> defaultApiHistory = Hawk.get%List<String> defaultApiHistory = Hawk.get%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 sed -i 's%// defaultApiHistory.add(defaultApiName);%defaultApiHistory.add(defaultApiName);%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
