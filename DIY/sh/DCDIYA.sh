3a88c97ef712e00201b4d7f0ded0c55a925d473c
wwz09 <wwz5124@gmail.com>
2025-10-05 10:34:14 +0800

    更新 DCDIYA.sh

--- DIY/sh/DCDIYA.sh
+++ DIY/sh/DCDIYA.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #添加PY支持
 
-sed -i 's%https://gitea.com/404ynnub/resource/raw/branch/main/boxsource.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+sed -i 's%https://gitea.com/404ynnub/resource/raw/branch/main/boxsource.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
 sed -i 's%http://www.饭太硬.net/tv/%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
 sed -i 's%http://www.饭太硬.net/tv%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
 sed -i 's%(Hawk.get(HawkConfig.API_URL, ""))%(Hawk.get(HawkConfig.API_URL, "https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/TXT/dc.json"))%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
 sed -i 's%url + URL.DEFAULT_STORE_API_URL%"https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/TXT/dc.json"%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
 sed -i 's%url + URL.DEFAULT_API_URL%"https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/main.json"%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
 sed -i 's%(HawkConfig.LIVE_URL, ""))%(HawkConfig.LIVE_URL, "https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/live2.txt"))%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
-sed -i 's%URL.DEFAULT_STORE_API_URL%"https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json"%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
+# sed -i 's%URL.DEFAULT_STORE_API_URL%"https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json"%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 # sed -i 's%// String defaultApiName = "自备份线路";%String defaultApiName = "自备份线路";%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 # sed -i 's%// String defaultApi = url + URL.DEFAULT_API_URL;%String defaultApi = "https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/main.json";%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 sed -i 's%// putDefault(HawkConfig.API%putDefault(HawkConfig.API%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
