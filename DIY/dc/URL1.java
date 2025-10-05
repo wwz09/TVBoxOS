ea3f6e1c181f68ca749f4b6e61a8efc5af5cf009
wwz09 <wwz5124@gmail.com>
2025-10-05 08:00:29 +0800

    sh

---
+++ DIY/dc/URL1.java
@@ -0,0 +1,12 @@
+package com.github.tvbox.osc.bbox.constant;
+
+public class URL {
+    public static String DOMAIN_NAME_PROXY = "https://raw.bunnyxyz.eu.org/";
+    
+    public static final String GITHUB_VERSION_PATH = "https://gitea.com/404ynnub/resource/raw/branch/main/version.json";
+    // 默认线路
+    public static final String DEFAULT_API_URL = "http://www.饭太硬.net/tv/";
+    // 默认仓库线路
+    public static final String DEFAULT_STORE_API_URL = "https://gitea.com/404ynnub/resource/raw/branch/main/boxsource.json";
+    // 默认直播线路
+    public static final String DEFAULT_LIVE_URL = "http://www.饭太硬.net/tv";
