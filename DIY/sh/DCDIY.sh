c6c22046e8900c25843698db1683a0648e8eaabe
wwz09 <wwz5124@gmail.com>
2025-10-05 08:02:57 +0800

    sh

--- DIY/sh/DCDIY.sh
+++ DIY/sh/DCDIY.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #添加PY支持
 
-sed -i 's%https://gitea.com/404ynnub/resource/raw/branch/main/boxsource.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
-sed -i 's%http://www.饭太硬.net/tv/%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
-sed -i 's%http://www.饭太硬.net/tv%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/a.txt%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
 sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
 sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
 sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/a.txt%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
